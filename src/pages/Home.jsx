6602e276e7998f0889454f06957624eac2829f4c
CL-so123 <104470045+CL-so123@users.noreply.github.com>
2026-04-29 16:20:00 -0400

    updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import {Link} from 'react-router-dom'
 import ReadPost from './ReadPost'
 import PostBox from '../components/PostBox'
+import { formatDistanceToNow } from 'date-fns'
 
 const home = () => {
     return(
         <div className="home">
             <h1>Forum Hub</h1>
+            <h2 className = "latest-posts">Latest Posts</h2>
              <ReadPost />
         </div>
     )
