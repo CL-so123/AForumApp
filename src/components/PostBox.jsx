0e0815aeaa19a4f607c9bcc8b2a1fb139f70749e
CL-so123 <104470045+CL-so123@users.noreply.github.com>
2026-04-29 16:04:28 -0400

    updates

--- src/components/PostBox.jsx
+++ src/components/PostBox.jsx
@@ -1,24 +1,29 @@
 import { Link } from 'react-router-dom'
 import { formatDistanceToNow } from 'date-fns'
+import './PostBox.css'
 
 const PostBox = ({ id, title, created_at }) => {
     return (
-        <div className="Box">
+        <Link to={`/post/${id}`} className= "box-link">
+            <div className="Box">
+                <h2> {title} </h2>
 
 
-            {created_at && (
-                <p className="date">
-                    Posted {formatDistanceToNow(new Date(created_at), {
-                        addSuffix: true
-                    })}
-                </p>
-            )}
-            <h2>
-                <Link to={`/post/${id}`}>
-                    {title}
-                </Link>
-            </h2>
-        </div>
+
+                {created_at && (
+                    <p className="date">
+                        Posted {formatDistanceToNow(new Date(created_at), {
+                            addSuffix: true
+                        })}
+                    </p>
+                )}
+
+
+
+
+
+            </div>
+        </Link>
     )
 }
 
